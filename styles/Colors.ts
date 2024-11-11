48bd2b71d44d3eb1d239a9b30122ddc9f8e3d2da
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-11-11 12:01:26 +0000

    feat(Color): Added color palette for light and dark mode.

---
+++ styles/Colors.ts
@@ -0,0 +1,16 @@
+export const colors = {
+
+    primaryLightColor: "#ffffff",
+    primaryDarkColor: "#931CA2",
+
+    cardsDarkMode: "#6A1372",
+    cardsLightMode: "#E9B4FA",
+
+    bottomLightColor: "#8C0D7A",
+    bottomDarkColor: "#f8f1ee",
+
+    textLightMode: "#000000",
+
+    tabBarActiveColor: "#FAB8F2"
+
+};
