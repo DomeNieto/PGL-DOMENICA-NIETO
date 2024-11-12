18678c9ead42cf40fec398d4b932c6c9012e9931
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-11-12 12:38:53 +0000

    feat(): Added redirectment to (tabs).

---
+++ app/portfolio/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+const Index = () => {
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" />
+        </Stack>
+    );
+};
+
+export default Index;
+
+
