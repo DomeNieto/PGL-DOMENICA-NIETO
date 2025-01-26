4f062ddcf1a33fed52dda9f3adc38e77b0caf19d
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-01-26 11:40:17 +0000

    feat(index): Added conditional rendering with useEffect() hook.

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,31 @@
-import { Redirect } from "expo-router";
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { getToken } from "../service/AsyncStorageService";
 
 export default function AppPage() {
-    return (
-        < Redirect href="/login/LoginPage" ></Redirect >
-    )
 
+    let isAuthenticated = false
+    const checkToken = async () => {
+        try {
+            const token = await getToken();
+            console.log(token)
+
+            if (token != null) {
+                isAuthenticated = true
+                router.navigate("/welcome/WelcomePage");
+
+            } else {
+                isAuthenticated = false
+                router.navigate("/login/LoginPage");
+            }
+        } catch (error) {
+            isAuthenticated = false;
+        }
+    };
+
+    useEffect(() => {
+        checkToken();
+    }, []);
+
+    return null;
 }
