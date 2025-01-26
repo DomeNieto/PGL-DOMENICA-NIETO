2887a4d5a45fc607beaf54003c25184edfa78143
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2025-01-26 11:06:51 +0000

    feat(): Added login service connected to the API.

--- service/UserService.ts
+++ service/UserService.ts
@@ -1,11 +1,11 @@
-import { UserRegisterType } from "../types/UserType";
+import { UserLoginType, UserRegisterType } from "../types/UserType";
 import { RequestService } from "./RequestService";
 
 
 const API_URL = "http://192.168.0.14:5000/auth"
 
 
-export const registerUser = async (data: UserRegisterType) => {
+const registerUser = async (data: UserRegisterType) => {
     try {
         const response = await fetch(
             `${API_URL}/register`,
@@ -13,10 +13,15 @@ export const registerUser = async (data: UserRegisterType) => {
         );
         const result = await response.json();
 
+        console.log("Status Code:", response.status);
+        console.log("Response Data:", result);
+
         return {
             status: response.status,
             datos: result,
         };
+
+
 
     } catch (error: any) {
         console.error("Error al registrar:", error);
@@ -27,7 +32,26 @@ export const registerUser = async (data: UserRegisterType) => {
     }
 };
 
+const loginUser = async (data: UserLoginType) => {
+    try {
+        const response = await fetch(`${API_URL}/login`, RequestService.initLoginRequest(data));
+
+        const result = await response.json();
+        console.log("Response Data:", result);
+
+        return {
+            status: response.status,
+            datos: result,
+        };
+    } catch (error: any) {
+        return {
+            status: 500,
+            error: error.message,
+        };
+    }
+};
 
 export const UserService = {
-    registerUser
+    registerUser,
+    loginUser
 }
